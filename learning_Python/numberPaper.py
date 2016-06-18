c59ab1e3a83666ae8d45fdff20f09cf75d25b605
flip3027 <flip3027@yahoo.com>
2016-06-17 20:10:14 -0400

    learned print table file 2:28am  6/9/2016

---
+++ learning_Python/numberPaper.py
@@ -0,0 +1,22 @@
+#! Python3
+import pyperclip
+import sys
+print('Enter how many numbers on the assignment:')
+
+if len(sys.argv) < 2:
+    print('Usage: py numberPaper.py[#] - copy numbers 1-#')
+    sys.exit()
+
+number = sys.argv[1]
+def assignNumber(num):
+    numList = []
+    for i in range(num + 1):
+        numList = numList + [i]
+    for j in numList:
+        numList[j] = str(j) + '\n'
+    text = '\n'.join(numList)
+    pyperclip.copy(text)
+    print('You have numbers copied 0-' + str(num))
+
+
+assignNumber(number)
