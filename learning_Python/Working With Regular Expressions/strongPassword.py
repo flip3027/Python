9be839b7cd3ff4f851565176763b39a0275dec14
flip3027 <flip3027@yahoo.com>
2016-06-24 04:44:08 -0400

    finished strongPassword (working with regular expressions)

--- learning_Python/Working With Regular Expressions/strongPassword.py
+++ learning_Python/Working With Regular Expressions/strongPassword.py
@@ -2,6 +2,7 @@
 
 import re, pyperclip
 
+#Todo: make a password regex
 passwordRegex = re.compile(r'''
 (?=.*[0-9])       # match zero or more Digits
 (?=.*[a-z])       # match zero or more lowercase letters
@@ -9,10 +10,13 @@ passwordRegex = re.compile(r'''
 (?=.*()).{8,}     # make sure password length is 8 characters long
 ''', re.VERBOSE)
 
+#Todo: make a screen prompt telling the user what to do
 print('''Enter a password: ''')
 print('\t (must have at least: 1 uppercase, 1 lowercase, 1 number and 8 characters long)')
+#Todo; put user's into a variable
 text = str(input())
-
+#Todo: Test user's input against the regex
+#Todo: if strong tell user then copy to clipboard else tell user to run again
 if passwordRegex.search(text) is not None:
     print('Password IS strong')
     print(text, ': has been copied to clipboard')
