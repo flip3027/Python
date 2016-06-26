7683e3ec1234bbb69a5e11d4eb5dd5310dd3c961
flip3027 <flip3027@yahoo.com>
2016-06-25 23:22:48 -0400

    made change Separator function

--- learning_Python/Useful Tools/changeSeparator.py
+++ learning_Python/Useful Tools/changeSeparator.py
@@ -2,9 +2,12 @@
 
 import pyperclip
 
-print('Please enter what you would like as a separator')
-separator = str(input())
-def changeSeparator(separator):
+print('Please enter a OLD separator')
+oldSeparator = str(input())
+print('Please enter a NEW separator')
+newSeparator = str(input())
+
+def changeSeparator():
     text = pyperclip.paste()
     '''
     TODO: put sentences into individual strings with the spilt module
@@ -12,12 +15,12 @@ def changeSeparator(separator):
     pass the strings into in a for loop that adds "separator" then the sentence then
     use join module at 'separator' because pyperclip.copy only take 1 string all together.
     '''
-    lines = text.split(' ')
+    lines = text.split(oldSeparator)
 
     for i in range(len(lines)):
         lines[i] = '' + lines[i]
-    text = separator.join(lines)
+    text = newSeparator.join(lines)
     pyperclip.copy(text)
-    print('The text has copy to the clipboard with ' + separator + "'s")
+    print('The text has copy to the clipboard with ' + newSeparator + "'s")
 
-changeSeparator(separator)
+changeSeparator()
