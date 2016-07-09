cc08fe720f8e470ae061a0f2e37313166cc964d8
flip3027 <flip3027@yahoo.com>
2016-07-09 04:54:39 -0400

    regex of strip done

--- learning_Python/Working With Regular Expressions/regexOfStrip.py
+++ learning_Python/Working With Regular Expressions/regexOfStrip.py
@@ -1,9 +1,15 @@
 #!python3
-
 import re
 
+def stripRegex(string, s=None):
+    if s == None:
+        s = '\s'
+    return re.sub(r'\A[{0}]+|[{0}]+\Z'.format(s), '', string)
 
-text = 'the quick brown fox jumped over the lazy dog'
-text2 = '             hellloooooooooooooo   today    '
 
-print(text.strip('  t o g'))
+text1 = 'the quick brown fox jumped over the lazy dog'
+text2 = '          hello today   '
+string2 = 'SpamSpamBaconSpamEggsSpamSpam'
+
+print(stripRegex(text2,))
+
