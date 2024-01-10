1c20d6cbf42365ab8739e3c378b7a6ee998cf634
flip3027 <flip3027@yahoo.com>
2024-01-10 02:00:44 -0500

    fix misspelled word and changeed range to 9

--- learning_Python/random catchAll/ArrayWithIntOutput.py
+++ learning_Python/random catchAll/ArrayWithIntOutput.py
@@ -1,8 +1,8 @@
-place = ("","London","Paris","Lisbon","Sydney","Baku","Pretoria","Buenos Aires","Toyoko")
+place = ("","London","Paris","Lisbon","Sydney","Baku","Pretoria","Buenos Aires","Tokyo")
 
 try:
-    num = int(input("Enter a whole number between 1 and 7"))
-    if num in range(1, 7):
+    num = int(input("Enter a whole number between 1 and 8:\n"))
+    if num in range(1, 9):
         print(place[num])
     else:
         print("The number can only be between 1 and 7 ")
