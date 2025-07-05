3f6105a34e07dfc54aa4c8ace35cda71f543abe4
flip3027 <flip3027@yahoo.com>
2025-07-05 04:29:59 -0400

    change random photo site

--- learning_Python/Chapter assignments/DownloadURLpictureFromWebAndAutoName/URLdownload.py
+++ learning_Python/Chapter assignments/DownloadURLpictureFromWebAndAutoName/URLdownload.py
@@ -9,4 +9,4 @@ def Download_Web_Image(url):
 #put a url of a picture from the web into the Function Below
 #Example: Download_Web_Image("https://s-media-cache-ak0.pinimg.com/236x/e9/3d/23/e93d233338465334f5263f2b311252bd.jpg")
 # Make sure you put qoutes at the beginning and end of the url
-Download_Web_Image("https://source.unsplash.com/random/300x200")
+Download_Web_Image("https://picsum.photos/1920/1080?random")
