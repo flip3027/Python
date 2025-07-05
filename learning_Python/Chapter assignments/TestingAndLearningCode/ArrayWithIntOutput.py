6e0307b850ae9d9434f742f9358e31d8f7dc67f4
flip3027 <flip3027@yahoo.com>
2025-07-05 05:32:26 -0400

    changed number of options

--- learning_Python/Chapter assignments/TestingAndLearningCode/ArrayWithIntOutput.py
+++ learning_Python/Chapter assignments/TestingAndLearningCode/ArrayWithIntOutput.py
@@ -1,10 +1,10 @@
-place = ("","London","Paris","Lisbon","Sydney","Baku","Pretoria","Buenos Aires","Toyoko")
+place = ("","London","Paris","Lisbon","Sydney","Baku","Pretoria","Buenos Aires","Tokyo")
 
 try:
-    num = int(input("Enter a whole number between 1 and 7"))
-    if num in range(1, 7):
+    num = int(input("Enter a whole number between 1-8: "))
+    if num in range(1, 9):
         print(place[num])
     else:
-        print("The number can only be between 1 and 7 ")
+        print("The number can only be between 1-8 ")
 except ValueError:
-    print('That was not a number between 1 and 7')
+    print('That was not a number between 1-8')
